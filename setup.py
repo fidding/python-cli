541c0c9ec09dc4c8cac71abaff8676621870df71
fidding <395455856@qq.com>
2018-05-31 21:05:05 +0800

    fixed setup

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    name='python-cli',
-    version='0.1',
+    name='fidding-cli',
+    version='0.4',
+    description='A python cli project',
     url='https://github.com/fidding/python-cli.git',
-    py_modules=['python-cli'],
+    packages=find_packages(),
+    author='fidding',
+    author_email='395455856@qq.com',
+    license='MIT',
     include_package_data=True,
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        fidding-cli=python_cli:cli
+        fidding-cli=python_cli.script.cli:cli
     ''',
 )
